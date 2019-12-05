69ac6ca9c4d51fb6cb97e5c0ae1f9365b9f520c8
Enrique Mayo <jmayo@unitedvirtualities.com>
2019-12-05 13:21:52 -0700

    01-Creating a Module

---
+++ ModulesOnNode/01-CreatingModule.js
@@ -0,0 +1,8 @@
+
+var sumTwoAndTwo = function (){
+    return 2+2;
+}
+
+//Así se indica cuando se quiere exportar un modulo, por lo general se le pone el mismo nombre de la función
+
+exports.sumTwoAndTwo = sumTwoAndTwo;
