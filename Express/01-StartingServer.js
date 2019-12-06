e9509d4bf5f7c35932a6bd973100183de9409e9e
Enrique Mayo <jmayo@unitedvirtualities.com>
2019-12-06 11:44:46 -0700

    Test Get Method

--- Express/01-StartingServer.js
+++ Express/01-StartingServer.js
@@ -3,6 +3,29 @@ const app = express();
 //Esta variable de entorno se utilizará para cambiar el puerto si está siendo utilizado o no
 const port = process.env.port || 3003
 
+
+//Declarando un array de coches
+var coches =[
+    {
+        id: 0, 
+        company: 'BMW',
+        model: 'X3',
+        year: '2020'
+    },
+    {
+        id: 1, 
+        company: 'Audi',
+        model: 'A1',
+        year: '2021'
+    },
+    {
+        id: 2, 
+        company: 'Mercedes',
+        model: 'Clase A',
+        year: '2022'
+    }
+]
+
 app.get('/', (req, res) =>{
     res.send('Hello World!')
 })
@@ -22,6 +45,21 @@ app.get('/api/cars/:company/:model', (req, res) => {
     res.send(req.params)
 }) 
 
+app.get('/api/cars/', (req, res)=>{
+    res.send(coches)
+})
+
+app.get('/api/cars/:company', (req, res) => {
+    //Busca dentro del arreglo si existe algún coche cuya compañía coincida con la compañía enviada por la URL
+    const coche = coches.find(coche => coche.company.toLowerCase() === req.params.company.toLowerCase())
+    if(!coche){
+        //Si no encuentra envía un status de 404 - NOT FOUND
+        res.status(404).send('No hay existencia de coches de esa marca')
+    }    
+    else{
+        res.send(coche)
+    }
+})
 
 app.listen(port)
     console.log('Server Listening on 3003')
